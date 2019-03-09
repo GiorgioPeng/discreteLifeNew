a3aa632d4ec4f8aa0ad86dc511abbb62e2e2e974
Giorgio <GiorgioPeng@github.com>
2019-03-09 22:36:55 +0800

    1

--- js/function.js
+++ js/function.js
@@ -15,6 +15,7 @@ var healthHeight = 0;
 var intelligenceHeight = 0;
 var wealthHeight = 0;
 var moralityHeight = 0;
+var numberOfDay = 1;
 
 function changeColor(number,type){
   if(parseInt(number.innerHTML.substr(0,number.innerHTML.length-1))>30)
@@ -54,7 +55,7 @@ function aHealthDWealth(){
 }
 //加 health 减 wealth    2
 
-function aEmotionDhealth(){
+function aEmotionDHealth(){
   emotionHeight = parseInt(emotion.style.height.substr(0,emotion.style.height.length-1));
   healthHeight = parseInt(health.style.height.substr(0,health.style.height.length-1));
   numberOfHealth.innerHTML = (parseInt(numberOfHealth.innerHTML.substr(0,numberOfHealth.innerHTML.length-1))-3)+"%";
@@ -324,3 +325,364 @@ function aEmotionDMorality(){
 }
 //加 emotion 减 morality 20
 // emotion.addEventListener("click",aEmotionDWealth,false);
+function primarySchoolChoose1(){
+  aWealthDIntelligence();
+  document.querySelector("#choose1").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+}
+
+function primarySchoolChoose2(){
+  aIntelligenceDWealth();
+  document.querySelector("#choose1").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+}
+
+function foodChoose1(){
+  aWealthDHealth();
+  document.querySelector("#choose2").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+}
+
+function foodChoose2(){
+  aHealthDWealth();
+  document.querySelector("#choose2").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+}
+
+function junkFood1(){
+  aEmotionDHealth();
+  document.querySelector("#choose3").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+}
+
+function junkFood2(){
+  aMoralityDEmotion();
+  document.querySelector("#choose3").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function chongzi1(){
+  aEmotionDHealth();
+  document.querySelector("#choose4").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function chongzi2(){
+  aEmotionDHealth();
+  document.querySelector("#choose4").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function money1(){
+  aMoralityDHealth();
+  document.querySelector("#choose5").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function money2(){
+  aMoralityDHealth();
+  document.querySelector("#choose5").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function bangbangtang1(){
+  aIntelligenceDEmotion();
+  document.querySelector("#choose6").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function bangbangtang2(){
+  aIntelligenceDEmotion();
+  document.querySelector("#choose6").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function marks1(){
+  aMoralityDHealth();
+  document.querySelector("#choose7").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function marks2(){
+  aIntelligenceDMorality();
+  document.querySelector("#choose7").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function game1(){
+  aEmotionDIntelligence();
+  document.querySelector("#choose8").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function game2(){
+  aIntelligenceDEmotion();
+  document.querySelector("#choose8").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function play1(){
+  aEmotionDIntelligence();
+  document.querySelector("#choose9").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function play2(){
+  aIntelligenceDEmotion();
+  document.querySelector("#choose9").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function help1(){
+  aEmotionDHealth();
+  document.querySelector("#choose10").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function help2(){
+  aIntelligenceDMorality();
+  document.querySelector("#choose10").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function road1(){
+  aIntelligenceDHealth();
+  document.querySelector("#choose11").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function road2(){
+  aWealthDEmotion();
+  document.querySelector("#choose11").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function qingshu1(){
+  aEmotionDIntelligence();
+  document.querySelector("#choose12").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function qingshu2(){
+  aIntelligenceDEmotion();
+  document.querySelector("#choose12").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function deng1(){
+  aIntelligenceDMorality();
+  aEmotionDWealth();
+  document.querySelector("#choose13").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function deng2(){
+  aMoralityDIntelligence();
+  document.querySelector("#choose13").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function rent1(){
+  aEmotionDWealth();
+  document.querySelector("#choose14").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
+
+function rent2(){
+  aIntelligenceDHealth();
+  document.querySelector("#choose14").style.display="none";
+  let randNumber = Math.floor(Math.random()*14)+1;
+  if(randNumber===1)
+    randNumber+=1;
+  document.getElementById("choose"+randNumber).style.display="block";
+  numberOfDay+=1;
+  rili.innerHTML = "你已经来到世间<br/><p id='day'>"+numberOfDay+"</p>天!!"
+
+}
